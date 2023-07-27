11edb6d648f4f11df316beb53749e634a28de286
otueteng <otuofemeteng@gmail.com>
2023-07-26 22:00:37 -0700

    task fixed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,22 @@
--- this script prepares a MySQL server for the project
--- create project developement database with the name : hbnb_dev_db
+-- create a MySQL server with:
+--   Database hbnb_dev_db.
+--   user hbnb_dev with password hbnb_dev_pwd in localhost.
+--   Grant all privileges for hbnb_dev on hbnb_dev_db.
+--   Grant SELECT privileges for hbnb_dev on performance.
+
+-- create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
 -- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
 -- with the password : hbnb_dev_pwd if it dosen't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
 -- granting all privileges to the new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
+
 -- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,22 @@
--- this script prepares a MySQL server for the project
--- create project testing database with the name : hbnb_test_db
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- creating new user named : hbnb_test with all privileges on the db hbnb_test_db
--- with the password : hbnb_test_pwd if it dosen't exist
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- granting the SELECT privilege for the user hbnb_test on the db performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- create a MySQL server with:
+--   Database hbnb_dev_db.
+--   user hbnb_dev with password hbnb_dev_pwd in localhost.
+--   Grant all privileges for hbnb_dev on hbnb_dev_db.
+--   Grant SELECT privileges for hbnb_dev on performance.
+
+-- create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
+-- with the password : hbnb_dev_pwd if it dosen't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- granting all privileges to the new user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- granting all privileges to the new user on hbnb_test_db
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
